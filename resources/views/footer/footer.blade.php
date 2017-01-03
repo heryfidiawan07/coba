669e8478fd5de53734de7fb4813ed6d0e61aa698
one07 <heryfidiawan07@gmail.com>
2017-01-04 01:56:08 +0700

    fix3 footer

--- resources/views/footer/footer.blade.php
+++ resources/views/footer/footer.blade.php
@@ -3,24 +3,18 @@
     <div class="row">
       <div class="col-md-6">
         <div class="media">
-          <a href="" class="pull-left">
-              <img src="/img/adm/adm.jpg" alt="fidawa.com" class="img-responsive img-circle">
-          </a>
-          <div class="media-body">
-              <div class="media-heading">
-                <p>Founder : Hery Fidiawan </p>
-              </div>
+          <div class="pull-left">
+            <img src="/img/adm/adm.jpg" alt="fidawa.com" class="img-responsive img-circle">
           </div>
+          <p style="margin-top: 15px;">Founder : Hery Fidiawan </p>
         </div>
       </div>
       <div class="col-md-6">
         <div class="text-center">
-          <ul>
             <li class="fotli"><a href="https://web.facebook.com/fidawadiskusi/">Facebook</a></li>
             <li class="fotli"><a href="/threads">Forum</a></li>
             <li class="fotli"><a href="/fjb">Fjb</a></li>
             <li class="fotli"><a href="/member">Member</a></li>
-          </ul>
         </div>
       </div>
     </div>
