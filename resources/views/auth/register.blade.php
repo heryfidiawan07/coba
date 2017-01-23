eefcbf9d1433083b6edbd39fc3643ec6c262a39a
one07 <heryfidiawan07@gmail.com>
2017-01-23 11:40:26 +0700

    mod spm with css

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -34,7 +34,7 @@
 
                             <div class="col-md-6">
                                 <input id="email" type="email" class="form-control input-sm" name="email" value="{{ old('email') }}" placeholder="Masukan alamat email yang masih di gunakan" required>
-                                <input type="hidden" name="spm">
+                                <input type="text" name="spm" id="spm">
 
                                 @if ($errors->has('email'))
                                     <span class="help-block">
